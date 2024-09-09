701e49c8878a260008cab38e7e2159cf97f1d77f
h1l3xx <alksmlkv@gmail.com>
2024-09-10 00:23:03 +0300

    feat(stat): drivers routes in UserController

--- stat-service/src/main/kotlin/com/fleetmate/stat/modules/user/dto/filter/StaffFilterDto.kt
+++ stat-service/src/main/kotlin/com/fleetmate/stat/modules/user/dto/filter/StaffFilterDto.kt
@@ -3,14 +3,20 @@ package com.fleetmate.stat.modules.user.dto.filter
 
 import com.fleetmate.lib.shared.modules.user.model.UserModel
 import com.fleetmate.stat.modules.order.data.dao.WorkDao.Companion.likeCond
+import com.fleetmate.stat.modules.order.data.dao.WorkDao.Companion.nullableLikeCond
 import kotlinx.serialization.Serializable
 import org.jetbrains.exposed.sql.Op
 import org.jetbrains.exposed.sql.SqlExpressionBuilder
+import org.jetbrains.exposed.sql.and
 
 @Serializable
 data class StaffFilterDto (
     val fullName: String? = null,
+    val phoneNumber: String? = null,
+    val licenceNumber: String? = null
 ) {
     val SqlExpressionBuilder.expressionBuilder: Op<Boolean> get() =
-        likeCond(fullName, UserModel.id neq 0, UserModel.fullName)
+        likeCond(fullName, UserModel.id neq 0, UserModel.fullName) and
+        likeCond(phoneNumber, UserModel.id neq 0, UserModel.phoneNumber) and
+        nullableLikeCond(licenceNumber, UserModel.id neq 0, UserModel.licenceNumber)
 }
